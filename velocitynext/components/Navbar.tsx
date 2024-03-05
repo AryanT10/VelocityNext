fffe542687a0ba7a7e6852d27d24950ab4ec352a
atuwar@myseneca.ca <atuwar@myseneca.ca>
2024-03-05 15:14:13 -0500

    add links to the footer,set Navbar

--- velocitynext/components/Navbar.tsx
+++ velocitynext/components/Navbar.tsx
@@ -8,7 +8,7 @@ const Navbar = () => {
 		<header className="w-full absolute z-10">
 			<nav className="max-2-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4">
 				<Link href="/" className="flex-center justify-center items-center">
-					<Image src="/logo.svg" alt="VelocityNext logo" width={118} height={18} className="object-contain" />
+					<Image src="/mc.svg" alt="VelocityNext logo" width={45} height={20} className="object-contain" />
 				</Link>
 
 				<CustomButton title="Sign In" btnType="button" containerStyles="text-primary-blue rounded-full bg-white min-w-[130px]" />
